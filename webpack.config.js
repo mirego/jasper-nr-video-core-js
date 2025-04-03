e7ba89fc00c5c1fff05ca37d39f8777dd5cd714d
rajeevkumar <rajeevkumar@newrelic.com>
2025-04-03 19:12:02 +0530

    feat: NPM publish through workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,41 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
 
-var pkg = require('./package.json')
+var pkg = require('./package.json');
 var license = '@license ' + pkg.license +
     '\n' + pkg.name + ' ' + pkg.version +
     '\nCopyright New Relic <http://newrelic.com/>' +
-    '\n@author ' + pkg.author
+    '\n@author ' + pkg.author;
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: pkg.name + ".min.js",
-    library: "nrvideo",
-    libraryTarget: "umd",
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [["@babel/preset-env", { targets: "defaults" }]],
+module.exports = [
+  {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "video-core.min.js",
+      library: "nrvideo",
+      libraryTarget: "umd",
+    },
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { targets: "defaults" }]],
+            },
           },
         },
-      },
+      ],
+    },
+    plugins: [
+      new webpack.BannerPlugin({
+        banner: license,
+        entryOnly: true,
+      }),
     ],
   },
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: license,
-      entryOnly: true,
-    }),
-  ],
-};
+];
