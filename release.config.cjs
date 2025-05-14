d22eb1ea0058ed76a868d81c4b00444a69aac0b9
rajeevkumar <rajeevkumar@newrelic.com>
2025-05-14 17:05:19 +0530

    fix: Added workflow

---
+++ release.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+    branches: [
+      { name: 'stable' },
+      { name: 'v*.*.*', prerelease: true },
+    ],
+    plugins: [
+      [
+        '@semantic-release/commit-analyzer',
+        {
+          preset: 'conventionalcommits',
+          releaseRules: [
+            { type: 'feat', release: 'major' },
+            { type: 'fix', release: 'patch' },
+            { type: 'perf', release: 'patch' },
+            { type: 'BREAKING CHANGE', release: 'major' },
+          ],
+          parserOpts: {
+            noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+          },
+        },
+      ],
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/github',
+      '@semantic-release/git',
+    ],
+  };
